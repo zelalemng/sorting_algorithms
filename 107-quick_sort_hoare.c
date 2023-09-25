9dc9f7e144af0d34289a0a153a632de727a6b055
zelalemng <zedneg@gmail.com>
2023-09-25 13:31:11 -0700

    Task_107

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,86 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
+void quick_sort_hoare(int *array, size_t size);
+
+/**
+ * swap_ints - swap two integer in array.
+ * @a: The first integer to swap.
+ * @b: the second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * hoare_partition - order a subset of an array of integers according
+ * to hoare partition scheme.
+ * @array: The array of integer.
+ * @size: the size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order
+ *
+ * Return: The final partition index.
+ */
+int hoare_partition(int *array, size_t size, int left, int right)
+{
+	int pivot, above, below;
+
+	pivot = array[right];
+	for (above = left - 1, below = right + 1; above < below;)
+	{
+		do {
+			above++;
+		} while (array[above] < pivot);
+		do {
+			below--;
+		} while (array[below] > pivot);
+
+		if (above < below)
+		{
+			swap_ints(array + above, array + below);
+			print_array(array, size);
+		}
+	}
+	return (above);
+}
+
+/**
+ * hoare_sort - Implement the quiksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: the size of the array
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ */
+void hoare_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = hoare_partition(array, size, left, right);
+		hoare_sort(array, size, left, part - 1);
+		hoare_sort(array, size, part, right);
+	}
+}
+
+/**
+ * quik_sort_hoare - Sort an array of integers in ascending
+ * order using the quicksort algorithm
+ * @array: an array of integers
+ * @size: The size of the array.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	hoare_sort(array, size, 0, size - 1);
+}
